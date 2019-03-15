f232ec44a9be66cdb93231eda99a127eee2610d0
Zuni <zuni.preece@whs.school.nz>
2019-03-15 19:11:04 +1300

    Initial Commit

---
+++ grand_piano/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grand_piano
+//
+//  Created by Zuni Preece on 15/03/19.
+//  Copyright © 2019 Zuni Preece. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
